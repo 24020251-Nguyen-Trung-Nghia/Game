826793ad7fea273ab2a234f2be3ecd3c91b42d9e
24020251-Nguyen-Trung-Nghia <24020251@vnu.edu.vn>
2025-10-09 22:40:43 +0700

    README

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.game {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.game to javafx.fxml;
-    exports org.example.game;
-}
